e34fdb3a199a20b856b0a2cf1723b5b1b59df528
unknown <mussienegash123456@gmail.com>
2024-08-14 05:26:34 -0700

    added product dart

---
+++ task6/lib/product.dart
@@ -0,0 +1,15 @@
+class Product {
+  String name;
+  int price;
+  String info;
+  String id;
+  String image;
+
+  Product({
+    required this.name,
+    required this.price,
+    required this.info,
+    required this.id,
+    required this.image,
+  });
+}
